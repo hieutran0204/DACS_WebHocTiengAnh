e8b9b56f8b9cafb0689fc06bcc92008dba305d36
sonson-56 <son.phamtk654123@gmail.com>
2025-04-30 11:03:27 +0700

    sua

--- WEB_TiengAnh/controllers/client/cover.controller.js
+++ WEB_TiengAnh/controllers/client/cover.controller.js
@@ -1,33 +1,33 @@
 const News = require('../../models/TOEIC/news.model');
 
-// exports.getCoverPage = async (req, res) => {
-//   try {
-//     const newsList = await News.find().limit(3); // Lấy 3 tin tức mới nhất từ CSDL
-//     res.render('client/pages/cover', {
-//       title: 'Trang Chủ | Học Tiếng Anh Online',
-//       newsList, // Truyền vào template
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).render('error');
-//   }
-// };
-
-// Lấy danh sách tin tức đã sắp xếp theo thời gian (mới nhất đầu tiên)
-exports.getNewsList = async (req, res) => {
+exports.getCoverPage = async (req, res) => {
   try {
-    const newsList = await News.find()
-      .sort({ createdAt: -1 }) // Sắp xếp giảm dần theo ngày tạo
-      .limit(3); // Chỉ lấy 3 tin mới nhất
-
-    res.render('home', {
-      newsList, // Truyền 3 tin mới nhất vào template
-      // Các dữ liệu khác...
+    const newsList = await News.find().limit(3); // Lấy 3 tin tức mới nhất từ CSDL
+    res.render('client/pages/cover', {
+      title: 'Trang Chủ | Học Tiếng Anh Online',
+      newsList, // Truyền vào template
     });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server Error');
+  } catch (error) {
+    console.error(error);
+    res.status(500).render('error');
   }
 };
+
+// // Lấy danh sách tin tức đã sắp xếp theo thời gian (mới nhất đầu tiên)
+// exports.getNewsList = async (req, res) => {
+//   try {
+//     const newsList = await News.find()
+//       .sort({ createdAt: -1 }) // Sắp xếp giảm dần theo ngày tạo
+//       .limit(3); // Chỉ lấy 3 tin mới nhất
+
+//     res.render('client/pages/cover', {
+//       newsList, // Truyền 3 tin mới nhất vào template
+//       // Các dữ liệu khác...
+//     });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send('Server Error');
+//   }
+// };
 
 // controllers/client/cover.controller.js
