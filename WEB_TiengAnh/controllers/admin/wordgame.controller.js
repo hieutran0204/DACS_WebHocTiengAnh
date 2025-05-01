5d1c99d612554f09a420960c9047bced59a549b4
NguyenHongPhuc-2455 <hongphuc092004@gmail.com>
2025-05-01 20:59:48 +0700

    deleteWord

--- WEB_TiengAnh/controllers/admin/wordgame.controller.js
+++ WEB_TiengAnh/controllers/admin/wordgame.controller.js
@@ -10,3 +10,9 @@ exports.viewWords = async (req, res) => {
   const words = await WordGame.find().lean();
   res.render('admin/pages/wordgame-manage', { words });
 };
+
+exports.deleteWord = async (req, res) => {
+  const { id } = req.params;
+  await WordGame.findByIdAndDelete(id);
+  res.redirect('/admin/wordgame/create');
+};
--- WEB_TiengAnh/routes/admin/wordgame.route.js
+++ WEB_TiengAnh/routes/admin/wordgame.route.js
@@ -5,5 +5,6 @@ const controller = require('../../controllers/admin/wordgame.controller');
 // Giao diện admin thêm từ
 router.get('/create', controller.viewWords);
 router.post('/create', controller.createWord);
+router.get('/delete/:id', controller.deleteWord);
 
 module.exports = router;
